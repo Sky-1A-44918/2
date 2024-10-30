f5470aaf99fa1022c7284b8c44ca4869510a6c30
Sky-1A-44918 <christofer.miguel.160209@alunosjc.sp.gov.br>
2024-10-30 08:39:52 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -93,10 +93,10 @@ function draw() {
   }
   
   // Calcula a nova largura e altura da imagem
-  let newWidth = imgWidth / 4; // Largura dividida por 4
-  let newHeight = imgHeight / 4;
-  let new2Width = imgWidth / 4; // Largura dividida por 4
-  let new2Height = imgHeight / 16;// Altura dividida por 4
+  let newWidth = imgWidth / 6; // Largura dividida por 4
+  let newHeight = imgHeight / 6;
+  let new2Width = imgWidth / 6; // Largura dividida por 4
+  let new2Height = imgHeight / 32;// Altura dividida por 4
   
   // Desenha a imagem no centro do canvas, com a posição y fixada em 450
   imageMode(CENTER); // Define o modo de desenho da imagem para o centro
